37881d86c8bc33e058c6cf65ef3f448478e33de5
Igor Perun <iperun23@gmail.com>
2018-05-01 11:54:36 -0400

    partciles component and variable added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
+import Rank from './components/Rank/Rank.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import './App.css';
+
+const particlesOptions = {
+    particles: {
+      number: {
+        value: 75,
+        density: {
+          enable: true,
+          value_area: 500
+        }
+      }
+    }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navigation />
-        <Logo />
-        {/*<ImageLinkForm />
-        <FaceRecognition /> */}
+      <Particles className='particles' params={particlesOptions}/>
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
       </div>
     );
   }

a1a2a1b45743c517010e9c80e33512ea2cfaad3b
Igor Perun <iperun23@gmail.com>
2018-05-23 15:22:46 -0400

    heroku port add ons

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ onInputChange = (event) => {
 
 onButtonSubmit = () => {
   this.setState({imageUrl: this.state.input})
-  fetch('http://localhost:3001/imageurl', {
+  fetch('https://desolate-meadow-26054.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -86,7 +86,7 @@ onButtonSubmit = () => {
     .then(response => response.json())
   .then(response => {
     if(response) {
-      fetch('http://localhost:3001/image', {
+      fetch('https://desolate-meadow-26054.herokuapp.com/image', {
         method: 'put',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
